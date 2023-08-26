47aa75f0a66e8d2db6b725ba09ab06f445749645
Pierre Guillen <hi@cerbero.dev>
2023-08-26 04:09:30 -0500

    Feature: Add go_router managment

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:io_extended_23/counter/counter.dart';
+import 'package:io_extended_23/gmail/gmail.dart';
+import 'package:io_extended_23/home/home.dart';
 import 'package:io_extended_23/l10n/l10n.dart';
 
 class App extends StatelessWidget {
@@ -7,7 +10,33 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    final router = GoRouter(
+      routes: [
+        GoRoute(
+          path: '/',
+          builder: (BuildContext context, GoRouterState state) {
+            return const HomePage();
+          },
+          routes: <RouteBase>[
+            GoRoute(
+              path: 'counter',
+              builder: (BuildContext context, GoRouterState state) {
+                return const CounterPage();
+              },
+            ),
+            GoRoute(
+              path: 'gmail',
+              builder: (BuildContext context, GoRouterState state) {
+                return const GmailPage();
+              },
+            ),
+          ],
+        ),
+      ],
+    );
+
+    return MaterialApp.router(
+      routerConfig: router,
       theme: ThemeData(
         appBarTheme: const AppBarTheme(color: Color(0xFF13B9FF)),
         colorScheme: ColorScheme.fromSwatch(
@@ -16,7 +45,6 @@ class App extends StatelessWidget {
       ),
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
-      home: const CounterPage(),
     );
   }
 }
